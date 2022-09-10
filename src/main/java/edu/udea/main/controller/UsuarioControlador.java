7df2e14b3957b22c8c6b4b34c4709654985d50dc
Andres Sanchez <fandressanchezm@gmail.com>
2022-09-09 20:02:16 -0500

    Actualizado todo el CRUD

--- src/main/java/edu/udea/main/controller/UsuarioControlador.java
+++ src/main/java/edu/udea/main/controller/UsuarioControlador.java
@@ -2,12 +2,13 @@ package edu.udea.main.controller;
 
 
 import edu.udea.main.business.GestorUsuario;
+import edu.udea.main.model.ObjetoRespuesta;
 import edu.udea.main.model.Usuario;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.lang.annotation.Target;
 import java.util.ArrayList;
 
 @RestController
@@ -15,10 +16,99 @@ public class UsuarioControlador {
 
     private GestorUsuario gestorUsuario = new GestorUsuario();
 
-    @GetMapping("/usuario")
+    // Metodo GET con QUERY PARAMS
+    @GetMapping("/usuarios")
     public ResponseEntity<ArrayList<Usuario>> getUsuario() {
-        return new ResponseEntity<>(gestorUsuario.getUsuarios(), HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(gestorUsuario.getUsuarios(), HttpStatus.OK);
     }
+
+
+    // Metodo GET Con la verificación de existencia de usuario
+    @GetMapping ("/usuario")
+    public ResponseEntity<Object> getUsuario (@RequestParam String id){
+
+        try {
+            Usuario usuario = gestorUsuario.getUsuario(id);
+            return new ResponseEntity<>(usuario,HttpStatus.OK);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage() ,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+
+    //Metodo GET con PATH PARAMS
+    @GetMapping("/usuario/{id}/edit")
+    public ResponseEntity<String> getUsuarioPath(@PathVariable String id){
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+
+
+
+    //Metodo POST con verificación de usuario
+    @PostMapping("/usuario")
+    public ResponseEntity<String> postUsuario(@RequestBody Usuario usuario){
+
+        try {
+            String mensaje = gestorUsuario.setUsuario(usuario);
+
+            return new ResponseEntity<>(mensaje, HttpStatus.OK);
+
+        } catch (Exception e) {
+           return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+
+    }
+
+        //PUT sirve para actualizar el objeto completo
+    @PutMapping("/usuario/{id}")
+    public ResponseEntity<ObjetoRespuesta> putUsuario(@RequestBody Usuario usuario_update, @PathVariable String id){
+        try {
+            Usuario usuario_bd = gestorUsuario.updateUsuarioAll(usuario_update, id);
+            return new ResponseEntity<>(new ObjetoRespuesta("Actualizacion Exitosa",usuario_bd), HttpStatus.OK);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ObjetoRespuesta(e.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+
+    //PATCH sirve para cuando solo se necesita actualizar una parte del objeto
+
+    @PatchMapping("/usuario/{id}")
+    public ResponseEntity<ObjetoRespuesta> patchUsuario(@RequestBody Usuario usuario_update, @PathVariable String id){
+        try {
+            Usuario usuario_bd = gestorUsuario.updateUsuario(usuario_update, id);
+            return new ResponseEntity<>(new ObjetoRespuesta("Actualizacion Exitosa", usuario_bd), HttpStatus.OK );
+
+        } catch (Exception e){
+            return new ResponseEntity<>(new ObjetoRespuesta(e.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR );
+        }
+    }
+
+
+
+
+    //DELETE Borrar cualquier usuario de la base de datos
+
+    @DeleteMapping("/usuario/{id}")
+    public ResponseEntity<ObjetoRespuesta> deleteUsuario(@PathVariable String id){
+        try {
+            String mensaje = gestorUsuario.deleteUsuario(id);
+
+            return new ResponseEntity<>(new ObjetoRespuesta("Eliminado Exitosamente", null), HttpStatus.OK );
+
+        } catch (Exception e) {
+
+            return new ResponseEntity<>(new ObjetoRespuesta(e.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR );
+
+        }
+
+    }
+
 
 
 }
---
+++ src/main/java/edu/udea/main/model/ObjetoRespuesta.java
@@ -0,0 +1,27 @@
+package edu.udea.main.model;
+
+public class ObjetoRespuesta {
+    private String mensajeRespuesta;
+    private Object object;
+
+    public ObjetoRespuesta(String mensajeRespuesta, Object object) {
+        this.mensajeRespuesta = mensajeRespuesta;
+        this.object = object;
+    }
+
+    public String getMensajeRespuesta() {
+        return mensajeRespuesta;
+    }
+
+    public void setMensajeRespuesta(String mensajeRespuesta) {
+        this.mensajeRespuesta = mensajeRespuesta;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+}
